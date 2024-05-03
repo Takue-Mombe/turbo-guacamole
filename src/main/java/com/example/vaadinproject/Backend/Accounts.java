c6c761a169fdc8a2009e1d00039f0db1104459c1
Takue <h220462g@hit.ac.zw>
2024-05-04 00:25:47 +0200

    adding models

---
+++ src/main/java/com/example/vaadinproject/Backend/Accounts.java
@@ -0,0 +1,4 @@
+package com.example.vaadinproject.Backend;
+
+public class Accounts {
+}
---
+++ src/main/java/com/example/vaadinproject/Backend/Patients.java
@@ -0,0 +1,13 @@
+package com.example.vaadinproject.Backend;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "patients")
+@Getter@Setter
+@AllArgsConstructor@NoArgsConstructor
+public class Patients {
+}
